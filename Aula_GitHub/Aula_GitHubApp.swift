c9d459b314828525c462a75bf4d862dd4c29add2
RaphaelIniesta <raphael_iniesta@yahoo.com>
2023-06-29 16:21:56 -0300

    Initial Commit

---
+++ Aula_GitHub/Aula_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_GitHubApp.swift
+//  Aula_GitHub
+//
+//  Created by Raphael Iniesta Reis on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
